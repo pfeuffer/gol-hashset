766cd124665be5d82fc5f2fed953e241d8cb5c0f
René Pfeuffer <rene@pfeuffer-web.de>
2013-12-29 22:15:56 +0100

    implemented: added cell is stored

--- src/main/java/de/pfeufferweb/gol/hashset/HashSetGol.java
+++ src/main/java/de/pfeufferweb/gol/hashset/HashSetGol.java
@@ -1,14 +1,16 @@
 package de.pfeufferweb.gol.hashset;
 
 import java.util.Collection;
-import java.util.Collections;
+import java.util.HashSet;
 
 import de.pfeufferweb.gol.benchmark.Gol;
 
 public class HashSetGol implements Gol {
+    Collection<Cell> cells = new HashSet<>();
 
     @Override
     public void addCell(int x, int y) {
+        cells.add(new Cell(x, y));
     }
 
     @Override
@@ -17,6 +19,6 @@ public class HashSetGol implements Gol {
     }
 
     public Collection<Cell> getCells() {
-        return Collections.emptySet();
+        return cells;
     }
 }
