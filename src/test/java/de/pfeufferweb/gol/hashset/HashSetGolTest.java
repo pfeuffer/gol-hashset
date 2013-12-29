8e6c3d776d6e2d916a6b609caaceb9ffc99c18a0
René Pfeuffer <rene@pfeuffer-web.de>
2013-12-29 22:14:41 +0100

    test: single cell is stored

--- src/test/java/de/pfeufferweb/gol/hashset/HashSetGolTest.java
+++ src/test/java/de/pfeufferweb/gol/hashset/HashSetGolTest.java
@@ -1,10 +1,19 @@
 package de.pfeufferweb.gol.hashset;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class HashSetGolTest {
     @Test
     public void emptyGolStaysEmpty() {
-        new HashSetGol().next().getCells().isEmpty();
+        assertTrue(new HashSetGol().next().getCells().isEmpty());
+    }
+
+    @Test
+    public void addedCellStored() {
+        HashSetGol underTest = new HashSetGol();
+        underTest.addCell(0, 0);
+        assertTrue(underTest.getCells().contains(new Cell(0, 0)));
     }
 }
